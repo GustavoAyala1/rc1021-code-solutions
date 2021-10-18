6dc727637942f4640b3300055084d8718b9b97af
Gustavo Ayala <gayalaand@gmail.com>
2021-10-17 21:29:05 -0700

    object manipulation 2 done

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,10 @@
 /* exported invert */
+
+const invert = source => {
+  const newObj = {};
+
+  for (const [prop, value] of Object.entries(source).reverse()) {
+    newObj[value] = prop;
+  }
+  return newObj;
+};
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,14 +1,32 @@
 /* exported omit */
-const omit = (source, keys) => {
-  // const newObj = {};
-  const rejected = {};
+// const omit = (source, keys) => {
+//   const newObj = {};
+//   const rejected = {};
 
-  for (const key in source) {
-    for (let i = 0; i < key.length; i++) {
-      if (key !== keys[i] || keys[i] === undefined) {
-        rejected[key] = source[key];
-        // console.log(key, keys[i]);
-      }
+//   for (let i = 0; i < keys.length; i++) {
+//     for (const property in source) {
+//       if (property === keys[i]) {
+//         newObj[property] = source[property];
+//       } else {
+//         rejected[property] = source[property];
+//       }
+//     }
+//   }
+//   console.log(rejected, newObj);
+//   return rejected;
+// };
+
+const omit = (source, keys) => {
+  const newObj = {};
+  const rejected = {};
+  let counter = 0;
+
+  for (const [prop, value] of Object.entries(source)) {
+    if (prop === keys[counter]) {
+      newObj[prop] = value;
+      counter++;
+    } else if (prop !== keys[counter]) {
+      rejected[prop] = value;
     }
   }
 
