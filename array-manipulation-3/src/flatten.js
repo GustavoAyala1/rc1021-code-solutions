8af269f13dca764b5a66d5c4f31d1b44c326358f
Gustavo Ayala <gayalaand@gmail.com>
2021-10-19 13:13:01 -0700

    array manipulation 3 with 2 pseudocode

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,21 +1,6 @@
 /* exported flatten */
 const flatten = array => {
-  const newArr = [];
+  const newArr = [].concat.apply([], array);
 
-  let i = 0;
-  let j = 0;
-
-  while (i < array.length) {
-    while (j < array[i].length) {
-      if (j < 1) {
-        newArr.push(array[i]);
-      } else {
-        newArr.push(array[i][j]);
-      }
-      j++;
-    }
-    i++;
-  }
+  return newArr;
 };
-
-// console.log(flatten([[1], [2], 3, 4, [5]]));
