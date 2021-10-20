b27266aa1b01f8654fb930fb8a171254c94d8558
Gustavo Ayala <gayalaand@gmail.com>
2021-10-20 10:53:10 -0700

    js forms changes

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,14 +1,11 @@
 const $formTwo = document.forms[0];
-const $formName = document.forms[0].name;
-const $formEmail = document.forms[0].email;
-const $formTextArea = document.forms[0].message;
 
 const handleSubmit = event => {
   event.preventDefault();
   const submitInfo = {
-    name: $formName.value,
-    email: $formEmail.value,
-    textArea: $formTextArea.value
+    name: $formTwo.name.value,
+    email: $formTwo.email.value,
+    textArea: $formTwo.message.value
   };
   console.log(submitInfo);
   $formTwo.reset();
