48766d857dda98f9e30d0619ad205b087ea2bd2c
Gustavo Ayala <gayalaand@gmail.com>
2021-10-21 11:54:24 -0700

    js typing tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -6,19 +6,22 @@ const typing = event => {
   const typed = event.key;
   const span = [...$span];
 
-  if (span[i].innerText === typed && i === 0) {
-    span[i].classList.add('correct');
-    span[i].classList.remove('currentL');
-    span[i + 1].classList.add('currentL');
+  if (span[i].innerHTML === typed && i === 0) {
+    span[0].classList.add('correct');
+    span[1].classList.add('currentL');
+    span[0].classList.remove('currentL');
+    span[0].classList.remove('wrong');
     i++;
-  } else if (span[i].innerText === typed && i !== 0) {
+  } else if (span[i].innerHTML === typed && i !== 0) {
     span[i].classList.add('correct');
+    span[i - 1].classList.add('correct');
     span[i + 1].classList.add('currentL');
     span[i].classList.remove('currentL');
-    span[i - 1].classList.remove('wrong');
+    span[i].classList.remove('wrong');
     i++;
-  } else {
-    span[i].classList.add = 'wrong';
+  } else if (span[i].innerHTML !== typed) {
+    span[i].classList.add('wrong');
+    span[i].classList.add('currentL');
   }
 };
 
