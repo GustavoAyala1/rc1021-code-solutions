7839d760c6654be7679bacfd81df8a238ff1841f
Gustavo Ayala <gayalaand@gmail.com>
2021-10-22 11:25:38 -0700

    continue work

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -22,58 +22,58 @@ const player4 = {
 };
 
 const deck = [
-  { rank: 2, suit: 'clubs' },
-  { rank: 3, suit: 'clubs' },
-  { rank: 4, suit: 'clubs' },
-  { rank: 5, suit: 'clubs' },
-  { rank: 6, suit: 'clubs' },
-  { rank: 7, suit: 'clubs' },
-  { rank: 8, suit: 'clubs' },
-  { rank: 9, suit: 'clubs' },
-  { rank: 10, suit: 'clubs' },
-  { rank: 'J', suit: 'clubs' },
-  { rank: 'Q', suit: 'clubs' },
-  { rank: 'K', suit: 'clubs' },
-  { rank: 'A', suit: 'clubs' },
-  { rank: 2, suit: 'diamonds' },
-  { rank: 3, suit: 'diamonds' },
-  { rank: 4, suit: 'diamonds' },
-  { rank: 5, suit: 'diamonds' },
-  { rank: 6, suit: 'diamonds' },
-  { rank: 7, suit: 'diamonds' },
-  { rank: 8, suit: 'diamonds' },
-  { rank: 9, suit: 'diamonds' },
-  { rank: 10, suit: 'diamonds' },
-  { rank: 'J', suit: 'diamonds' },
-  { rank: 'Q', suit: 'diamonds' },
-  { rank: 'K', suit: 'diamonds' },
-  { rank: 'A', suit: 'diamonds' },
-  { rank: 2, suit: 'hearts' },
-  { rank: 3, suit: 'hearts' },
-  { rank: 4, suit: 'hearts' },
-  { rank: 5, suit: 'hearts' },
-  { rank: 6, suit: 'hearts' },
-  { rank: 7, suit: 'hearts' },
-  { rank: 8, suit: 'hearts' },
-  { rank: 9, suit: 'hearts' },
-  { rank: 10, suit: 'hearts' },
-  { rank: 'J', suit: 'hearts' },
-  { rank: 'Q', suit: 'hearts' },
-  { rank: 'K', suit: 'hearts' },
-  { rank: 'A', suit: 'hearts' },
-  { rank: 2, suit: 'spades' },
-  { rank: 3, suit: 'spades' },
-  { rank: 4, suit: 'spades' },
-  { rank: 5, suit: 'spades' },
-  { rank: 6, suit: 'spades' },
-  { rank: 7, suit: 'spades' },
-  { rank: 8, suit: 'spades' },
-  { rank: 9, suit: 'spades' },
-  { rank: 10, suit: 'spades' },
-  { rank: 'J', suit: 'spades' },
-  { rank: 'Q', suit: 'spades' },
-  { rank: 'K', suit: 'spades' },
-  { rank: 'A', suit: 'spades' }
+  { rank: 2, suit: 'clubs', point: 2 },
+  { rank: 3, suit: 'clubs', point: 3 },
+  { rank: 4, suit: 'clubs', point: 4 },
+  { rank: 5, suit: 'clubs', point: 5 },
+  { rank: 6, suit: 'clubs', point: 6 },
+  { rank: 7, suit: 'clubs', point: 7 },
+  { rank: 8, suit: 'clubs', point: 8 },
+  { rank: 9, suit: 'clubs', point: 9 },
+  { rank: 10, suit: 'clubs', point: 10 },
+  { rank: 'J', suit: 'clubs', point: 10 },
+  { rank: 'Q', suit: 'clubs', point: 10 },
+  { rank: 'K', suit: 'clubs', point: 10 },
+  { rank: 'A', suit: 'clubs', point: 11 },
+  { rank: 2, suit: 'diamonds', point: 2 },
+  { rank: 3, suit: 'diamonds', point: 3 },
+  { rank: 4, suit: 'diamonds', point: 4 },
+  { rank: 5, suit: 'diamonds', point: 5 },
+  { rank: 6, suit: 'diamonds', point: 6 },
+  { rank: 7, suit: 'diamonds', point: 7 },
+  { rank: 8, suit: 'diamonds', point: 8 },
+  { rank: 9, suit: 'diamonds', point: 9 },
+  { rank: 10, suit: 'diamonds', point: 10 },
+  { rank: 'J', suit: 'diamonds', point: 10 },
+  { rank: 'Q', suit: 'diamonds', point: 10 },
+  { rank: 'K', suit: 'diamonds', point: 10 },
+  { rank: 'A', suit: 'diamonds', point: 11 },
+  { rank: 2, suit: 'hearts', point: 2 },
+  { rank: 3, suit: 'hearts', point: 3 },
+  { rank: 4, suit: 'hearts', point: 4 },
+  { rank: 5, suit: 'hearts', point: 5 },
+  { rank: 6, suit: 'hearts', point: 6 },
+  { rank: 7, suit: 'hearts', point: 7 },
+  { rank: 8, suit: 'hearts', point: 8 },
+  { rank: 9, suit: 'hearts', point: 9 },
+  { rank: 10, suit: 'hearts', point: 10 },
+  { rank: 'J', suit: 'hearts', point: 10 },
+  { rank: 'Q', suit: 'hearts', point: 10 },
+  { rank: 'K', suit: 'hearts', point: 10 },
+  { rank: 'A', suit: 'hearts', point: 11 },
+  { rank: 2, suit: 'spades', point: 2 },
+  { rank: 3, suit: 'spades', point: 3 },
+  { rank: 4, suit: 'spades', point: 4 },
+  { rank: 5, suit: 'spades', point: 5 },
+  { rank: 6, suit: 'spades', point: 6 },
+  { rank: 7, suit: 'spades', point: 7 },
+  { rank: 8, suit: 'spades', point: 8 },
+  { rank: 9, suit: 'spades', point: 9 },
+  { rank: 10, suit: 'spades', point: 10 },
+  { rank: 'J', suit: 'spades', point: 10 },
+  { rank: 'Q', suit: 'spades', point: 10 },
+  { rank: 'K', suit: 'spades', point: 10 },
+  { rank: 'A', suit: 'spades', point: 11 }
 ];
 
 const firstRandom = () => {
@@ -85,41 +85,61 @@ const secondRandom = () => {
 
 let result1;
 let result2;
-// const point = 0;
+const startDealing = () => {
+  for (let i = 0; i < 4; i++) {
+    result1 = firstRandom();
+    result2 = secondRandom();
+    if (i === 0) {
+      player1.hand.push(deck[result1].rank);
+      player1.hand.push(deck[result1].suit);
+      player1.score += deck[result1].point;
+      deck.splice(result1, 1);
 
-for (let i = 0; i < 4; i++) {
-  result1 = firstRandom();
-  result2 = secondRandom();
-  if (i === 0) {
-    player1.hand.push(deck[result1].rank);
-    player1.hand.push(deck[result1].suit);
+      player1.hand.push(deck[result2].rank);
+      player1.hand.push(deck[result2].suit);
+      player1.score += deck[result2].point;
+      deck.splice(result2, 1);
+    } else if (i === 1) {
 
-    player1.hand.push(deck[result2].rank);
-    player1.hand.push(deck[result2].suit);
-  } else if (i === 1) {
-    player2.hand.push(deck[result1].rank);
-    player2.hand.push(deck[result1].suit);
+      player2.hand.push(deck[result1].rank);
+      player2.hand.push(deck[result1].suit);
+      player2.score += deck[result1].point;
+      deck.splice(result1, 1);
 
-    player2.hand.push(deck[result2].rank);
-    player2.hand.push(deck[result2].suit);
-  } else if (i === 2) {
-    player3.hand.push(deck[result1].rank);
-    player3.hand.push(deck[result1].suit);
+      player2.hand.push(deck[result2].rank);
+      player2.hand.push(deck[result2].suit);
+      player2.score += deck[result2].point;
+      deck.splice(result2, 1);
 
-    player3.hand.push(deck[result2].rank);
-    player3.hand.push(deck[result2].suit);
-  } else if (i === 3) {
-    player4.hand.push(deck[result1].rank);
-    player4.hand.push(deck[result1].suit);
+    } else if (i === 2) {
+      player3.hand.push(deck[result1].rank);
+      player3.hand.push(deck[result1].suit);
+      player3.score += deck[result1].point;
+      deck.splice(result1, 1);
 
-    player4.hand.push(deck[result2].rank);
-    player4.hand.push(deck[result2].suit);
+      player3.hand.push(deck[result2].rank);
+      player3.hand.push(deck[result2].suit);
+      player3.score += deck[result2].point;
+      deck.splice(result2, 1);
+
+    } else if (i === 3) {
+      player4.hand.push(deck[result1].rank);
+      player4.hand.push(deck[result1].suit);
+      player4.score += deck[result1].point;
+      deck.splice(result1, 1);
+
+      player4.hand.push(deck[result2].rank);
+      player4.hand.push(deck[result2].suit);
+      player4.score += deck[result2].point;
+      deck.splice(result2, 1);
+
+    }
+
   }
-  // if (i === 0) {
-  // }
-}
+};
 
-console.log(player1.hand);
-console.log(player2.hand);
-console.log(player3.hand);
-console.log(player4.hand);
+startDealing();
+console.log(player1.hand, player1.score);
+console.log(player2.hand, player2.score);
+console.log(player3.hand, player3.score);
+console.log(player4.hand, player4.score);
