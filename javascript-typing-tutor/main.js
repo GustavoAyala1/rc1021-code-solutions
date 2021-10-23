140d83f09e2d7328188fb1a8e0a2317a3cabab87
Gustavo Ayala <gayalaand@gmail.com>
2021-10-22 18:03:27 -0700

    added accuracy and redo button

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,6 +1,9 @@
 const $span = document.querySelectorAll('span');
+const $congrats = document.querySelector('.congratsCont');
+const $redoBtn = document.querySelector('.redoBtn');
 
 let i = 0;
+let errors = 0;
 
 const typing = event => {
   const typed = event.key;
@@ -12,6 +15,13 @@ const typing = event => {
     span[0].classList.remove('currentL');
     span[0].classList.remove('wrong');
     i++;
+  } else if (span[i].innerHTML === typed && i === (span.length - 1)) {
+    span[i].classList.add('correct');
+    span[i - 1].classList.add('correct');
+    span[i].classList.remove('currentL');
+    span[i].classList.remove('wrong');
+    $congrats.innerText = `Congratulations you had ${(25 - errors) / 25 * 100}% typing accuracy`;
+    $redoBtn.classList.remove('hidden');
   } else if (span[i].innerHTML === typed && i !== 0) {
     span[i].classList.add('correct');
     span[i - 1].classList.add('correct');
@@ -22,7 +32,23 @@ const typing = event => {
   } else if (span[i].innerHTML !== typed) {
     span[i].classList.add('wrong');
     span[i].classList.add('currentL');
+    errors++;
   }
 };
 
+const redo = event => {
+  for (let j = 0; j < $span.length; j++) {
+    if (j !== 0) {
+      $span[j].className = '';
+    } else if (j === 0) {
+      $span[j].className = 'currenL';
+    }
+  }
+  i = 0;
+  errors = 0;
+  $congrats.innerText = '';
+  $redoBtn.classList.add('hidden');
+};
+
 document.addEventListener('keydown', typing);
+$redoBtn.addEventListener('click', redo);
