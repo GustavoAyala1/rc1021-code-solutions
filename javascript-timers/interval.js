271b9df7e34c51c94f6aec0d4bc64cd0b7103967
Gustavo Ayala <gayalaand@gmail.com>
2021-11-03 10:31:10 -0700

    javascript-timers adding a clearInterval

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -2,9 +2,10 @@ const h1 = document.querySelector('h1');
 
 let countdown = 4;
 
-setInterval(() => {
+const earth = setInterval(() => {
   if (countdown === 0) {
     h1.innerText = '~Earth Beeeelooowww Us~';
+    clearInterval(earth);
   } else {
     h1.innerText = `${countdown--}`;
   }
