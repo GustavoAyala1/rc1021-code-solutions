c55c90026f2c4bdfaf74ad6674068fab81ff9ef5
Gustavo Ayala <gayalaand@gmail.com>
2021-11-05 12:41:26 -0700

    js ajax

---
+++ javascript-ajax/users.js
@@ -0,0 +1,19 @@
+const $ul = document.querySelector('#user-list');
+
+function getUserData() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', () => {
+    console.log(xhr.status);
+    console.log(xhr.response);
+    for (let i = 0; i < xhr.response.length; i++) {
+      const li = document.createElement('li');
+      li.textContent = xhr.response[i].name;
+      $ul.appendChild(li);
+    }
+  });
+  xhr.send();
+}
+
+getUserData();
