705a0d6cd15e03426d5ede3ac2065ed6cd2bb7e9
Gustavo Ayala <gayalaand@gmail.com>
2021-11-29 15:19:40 -0800

    simplified code and added a callback

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const notesJson = require('./data.json');
 
+const updateJson = whatEvent => {
+  fs.writeFile('data.json', JSON.stringify(notesJson), err => {
+    if (err) throw err;
+    console.log(`${whatEvent} complete`);
+  });
+};
+
 if (process.argv[2] === 'read') {
   for (const [key, value] of Object.entries(notesJson.notes)) {
     console.log(`${key}: ${value}`);
@@ -8,20 +15,11 @@ if (process.argv[2] === 'read') {
 } else if (process.argv[2] === 'create') {
   notesJson.notes[`${notesJson.nextId}`] = process.argv[3];
   notesJson.nextId++;
-  fs.writeFile('data.json', JSON.stringify(notesJson), err => {
-    if (err) throw err;
-    console.log('create complete');
-  });
+  updateJson('create');
 } else if (process.argv[2] === 'delete') {
   delete notesJson.notes[process.argv[3]];
-  fs.writeFile('data.json', JSON.stringify(notesJson), err => {
-    if (err) throw err;
-    console.log('delete complete');
-  });
+  updateJson('delete');
 } else if (process.argv[2] === 'update') {
   notesJson.notes[process.argv[3]] = process.argv[4];
-  fs.writeFile('data.json', JSON.stringify(notesJson), err => {
-    if (err) throw err;
-    console.log('delete complete');
-  });
+  updateJson('update');
 }
