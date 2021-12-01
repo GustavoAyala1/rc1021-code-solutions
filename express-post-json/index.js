b38a500cf9dedee2ef47884d300579b1216b7192
Gustavo Ayala <gayalaand@gmail.com>
2021-12-01 15:36:45 -0800

    express-post-json

---
+++ express-post-json/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+let nextId = 1;
+
+const grades = {};
+
+app.use(express.json());
+
+app.post("/api/grades", (req, res) => {
+  const name = req.body.name;
+  const course = req.body.course;
+  const grade = req.body.grade;
+  const id = nextId;
+
+  grades[nextId] = { id, name, course, grade };
+  nextId++;
+  res.status(201);
+  res.send(grades);
+});
+
+app.get("/api/grades", (req, res) => {
+  console.log("in get");
+  const gradesArray = [];
+  for (let id in grades) {
+    gradesArray.push(grades[id]);
+  }
+  res.json(gradesArray);
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
