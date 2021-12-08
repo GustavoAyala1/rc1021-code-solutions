67583d54dcc3f41baebabaae5e1cf4bfefc21657
Gustavo Ayala <gayalaand@gmail.com>
2021-12-07 18:19:52 -0800

    fixed minor mistake in put

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -34,7 +34,7 @@ app.post('/api/grades', (req, res) => {
   const { name, course, score } = req.body;
 
   const sql = `
-    insert into "grades" ("name","course","score")
+    insert into "grades" ("nme","course","score")
     values ('${name}','${course}','${score}')
     returning *;
   `;
@@ -50,27 +50,52 @@ app.post('/api/grades', (req, res) => {
   });
 });
 
-// app.put('/api/grades/:gradeId', (req, res) => {
-//   const { name, course, score } = req.body;
-//   const id = +req.params.gradeId;
+app.put('/api/grades/:gradeId', (req, res) => {
+  const { name, course, score } = req.body;
+  const id = +req.params.gradeId;
 
-//   const sql = `
-//   insert into "grades" ("name","course","score")
-//   values ('${name}','${course}','${score}')
-//   returning *;
-//   `;
+  const sqlChecking = `
+  select "name","course","score"
+  from "grades"
+  where "gradeId" = ${id};
+  `;
 
-//   if (!name || !course || !score || score < 0 || score > 100) {
-//     res.status(400).json({ error: 'Name, Course, and Score are required' });
-//   }
+  const sql = `
+  update "grade"
+  set "name"='${name}',"course"='${course}',"score"='${score}'
+  where "gradeId"=${id}
+  returning *;
+  `;
 
-// });
+  if (!name || !course || !score || score < 0 || score > 100) {
+    res.status(400).json({ error: 'Name, Course, and Score are required' });
+  } else {
+    db.query(sqlChecking)
+      .then(result => {
+        if (result.rows.length !== 0) {
+          db.query(sql)
+            .then(results => {
+              res.json({ test: 'successfully updated', results: results.rows });
+            })
+          // eslint-disable-next-line node/handle-callback-err
+            .catch(error => { res.status(500).json({ error: 'An unexpected error occurred' }); });
+        } else {
+          throw new Error();
+        }
+      })
+      // eslint-disable-next-line node/handle-callback-err
+      .catch(error => {
+        res.status(404).json({ error: 'Student with this gradeId does not exist' });
+      });
+  }
+
+});
 
 app.delete('/api/grades/:gradeId', (req, res) => {
   const id = +req.params.gradeId;
 
   const sql = `
- delete from "grades"
+ delete from "grads"
  where "gradeId"=${id};
   `;
 
@@ -82,18 +107,18 @@ app.delete('/api/grades/:gradeId', (req, res) => {
 
   if (id < 0 || isNaN(id)) {
     res.status(400).json({ error: 'Please enter a valid gradeId' });
-  }
-
-  db.query(sqlChecking).then(result => {
-    db.query(sql)
-      .then(results => { res.json({ test: 'successfully deleted' }); })
+  } else {
+    db.query(sqlChecking).then(result => {
+      db.query(sql)
+        .then(results => { res.json({ test: 'successfully deleted' }); })
       // eslint-disable-next-line node/handle-callback-err
-      .catch(error => { res.status(500).json({ error: 'An unexpected error occurred' }); });
+        .catch(error => { res.status(500).json({ error: 'An unexpected error occurred' }); });
 
-  // eslint-disable-next-line node/handle-callback-err
-  }).catch(error => {
-    res.status(404).json({ error: 'Student with this gradeId does not exist' });
-  });
+      // eslint-disable-next-line node/handle-callback-err
+    }).catch(error => {
+      res.status(404).json({ error: 'Student with this gradeId does not exist' });
+    });
+  }
 
 });
 
