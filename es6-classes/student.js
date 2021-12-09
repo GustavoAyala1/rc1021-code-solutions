40b2dbf0ee763ddc984e182046a8f4b094049ff5
Gustavo Ayala <gayalaand@gmail.com>
2021-12-08 18:06:37 -0800

    es6-classes

---
+++ es6-classes/student.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-expressions */
+/* exported Student */
+
+class Student {
+  constructor(firstName, lastName, subject) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.subject = subject;
+  }
+
+  getFullName() {
+
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  getIntroduction() {
+    return `Hello, my name is ${this.getFullName()} and I am studying ${this.subject}.`;
+  }
+}
