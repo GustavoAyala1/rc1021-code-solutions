501675e894d292a61b01ab501caf998520c794a8
Gustavo Ayala <gayalaand@gmail.com>
2021-12-10 15:08:50 -0800

    react-function-components

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const div = document.querySelector('#root');
-function CustomButton() {
-  return (
-    <button>Click Me!</button>
-  );
-}
 
-ReactDOM.render(CustomButton(), div);
+const CustomButton = <button>Click Me!</button>;
+
+ReactDOM.render(CustomButton, div);
