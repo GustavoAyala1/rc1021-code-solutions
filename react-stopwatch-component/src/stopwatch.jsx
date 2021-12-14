bb1b32d0b171307e7aaf82ef3e246bbfda7cf1f4
Gustavo Ayala <gayalaand@gmail.com>
2021-12-14 10:13:19 -0800

    continue working on reseting state

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -4,33 +4,34 @@ class StopWatch extends React.Component {
   constructor(props) {
     super(props);
     this.state = { time: 0, playing: false };
+    this.handleTimer = null;
     this.handleClick = this.handleClick.bind(this);
-    this.handleTimer = this.handleTimer.bind(this);
   }
 
   handleClick() {
+
+    if (this.state.time > 1 && !this.state.playing) { this.resetState(); }
     this.setState({ time: this.state.time + 1, playing: !this.state.playing });
-    if (!this.state.time > 1) this.handleTimer();
 
   }
 
-  handleTimer() {
-    let autoCounter;
-    if (this.state.playing) {
-      autoCounter = setInterval(() => {
-        this.setState({ time: this.state.time + 1 });
-      }, 1000);
-      return;
-    }
+  handleCounter() {
+    this.handleTimer = setInterval(() => {
+      this.setState({ time: this.state.time + 1 });
+    }, 1000);
+  }
 
-    clearInterval(autoCounter);
+  resetState() {
+    this.setState({ time: 0, playing: true });
+    // this.handleClick();
 
   }
 
   render() {
     const { time, playing } = this.state;
     // console.log(time, playing);
-    if (time === 1) this.handleTimer();
+    if (time === 1) { this.handleCounter(); } else if (!playing) { clearInterval(this.handleTimer); }
+
     return (
     <div className='stopDiv'>
       <div className='border'>{time} <span className='play'></span></div>
