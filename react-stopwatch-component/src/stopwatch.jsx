7eb33e9fb565f4c49afb4631bfa9c7dce52ee577
Gustavo Ayala <gayalaand@gmail.com>
2021-12-13 18:48:40 -0800

    counter works

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -3,15 +3,38 @@ import React from 'react';
 class StopWatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = ({ timer: 0 });
-    // this.handleClick = this.handleClick.bind(this);
+    this.state = { time: 0, playing: false };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleTimer = this.handleTimer.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ time: this.state.time + 1, playing: !this.state.playing });
+    if (!this.state.time > 1) this.handleTimer();
+
+  }
+
+  handleTimer() {
+    let autoCounter;
+    if (this.state.playing) {
+      autoCounter = setInterval(() => {
+        this.setState({ time: this.state.time + 1 });
+      }, 1000);
+      return;
+    }
+
+    clearInterval(autoCounter);
+
   }
 
   render() {
+    const { time, playing } = this.state;
+    // console.log(time, playing);
+    if (time === 1) this.handleTimer();
     return (
     <div className='stopDiv'>
-      <div className='border'>5 <span className='play'></span></div>
-      <button className='btn stopBtn'></button>
+      <div className='border'>{time} <span className='play'></span></div>
+      <button className='btn' onClick={this.handleClick}>{!playing ? 'PLAY!' : 'STOP!'}</button>
     </div>
     );
   }
